dfcf37571491d2654f50b4891a8c160cdd0c89f7
XZ1048576 <sylvain.lovis@gmail.com>
2020-04-01 18:44:10 +0200

    boutons et fonctions pour ajouter/retirer des lignes/colonnes

--- map_creator/map.h
+++ map_creator/map.h
@@ -9,15 +9,131 @@ UINT8 Map_GetAt(Map map,int x,int y){
     UINT8 mask=0xf0>>((x&1)<<2);
     return (result&mask)>>(((x^1)&1)<<2);
 }
-UINT8 Map_SetAt(Map map,int x,int y,UINT8 value){
+void Map_SetAt(Map map,int x,int y,UINT8 value){
     if(x>=map.width || y>=map.height || (value>8)){
-        return 9;
+        return;
     }
     UINT8* value_to_change=(map.data+((map.width+1)>>1)*y+(x>>1));
     UINT8 mask=0xf<<((x&1)<<2);
     value=value<<(((x^1)&1)<<2);
     *value_to_change=((*value_to_change&mask)|value);
-    return 0;
+    return;
+}
+void Map_insert_col(Map *map,int x){
+    if(map->width==255){
+        return;
+    }
+    map->width++;
+    if(map->width&1){
+        UINT8* new_data=malloc(((map->width+1)>>1)*map->height);
+        UINT8* old_data=map->data;
+        map->data=new_data;
+        for(int i=0;i<map->height;i++){
+            for(int j=0;j<map->width;j++){
+                if(j>x){
+                    map->data=old_data;
+                    map->width--;
+                    UINT8 value=Map_GetAt(*map,j-1,i);
+                    map->data=new_data;
+                    map->width++;
+                    Map_SetAt(*map,j,i,value);
+                } else if(j==x){
+                    Map_SetAt(*map,j,i,map->default_);
+                } else {
+                    map->data=old_data;
+                    map->width--;
+                    UINT8 value=Map_GetAt(*map,j,i);
+                    map->data=new_data;
+                    map->width++;
+                    Map_SetAt(*map,j,i,value);
+                }
+            }
+        }
+        free(old_data);
+    } else {
+        for(int i=0;i<map->height;i++){
+            for(int j=map->width-1;j>x;j--){
+                Map_SetAt(*map,j,i,Map_GetAt(*map,j-1,i));
+            }
+            Map_SetAt(*map,x,i,map->default_);
+        }
+    }
+}
+void Map_delete_col(Map *map,int x){
+    if(map->width==1){
+        return;
+    }
+    map->width--;
+    if(!(map->width&1)){
+        UINT8* new_data=malloc(((map->width+1)>>1)*map->height);
+        UINT8* old_data=map->data;
+        map->data=new_data;
+        for(int i=0;i<map->height;i++){
+            for(int j=0;j<map->width;j++){
+                if(j>=x){
+                    map->data=old_data;
+                    map->width++;
+                    UINT8 value=Map_GetAt(*map,j+1,i);
+                    map->data=new_data;
+                    map->width--;
+                    Map_SetAt(*map,j,i,value);
+                } else {
+                    map->data=old_data;
+                    map->width++;
+                    UINT8 value=Map_GetAt(*map,j,i);
+                    map->data=new_data;
+                    map->width--;
+                    Map_SetAt(*map,j,i,value);
+                }
+            }
+        }
+        free(old_data);
+    } else {
+        map->width++;
+        for(int i=0;i<map->height;i++){
+            for(int j=x;j<map->width;j++){
+                Map_SetAt(*map,j,i,Map_GetAt(*map,j+1,i));
+            }
+        }
+        map->width--;
+    }
+}
+void Map_insert_line(Map *map,int y){
+    if(map->height==255){
+        return;
+    }
+    map->height++;
+    int bwidth=(map->width+1)>>1;
+    UINT8*new_data=malloc(bwidth*map->height);
+    UINT8*old_data=map->data;
+    for(int i=0;i<y*bwidth;i++){
+        new_data[i]=old_data[i];
+    }
+    for(int i=0;i<bwidth;i++){
+        new_data[i+bwidth*y]=0x11*map->default_;
+    }
+    for(int i=0;i<(map->height-y-1)*bwidth;i++){
+        new_data[i+bwidth*(y+1)]=old_data[i+bwidth*y];
+    }
+    map->data=new_data;
+    free(old_data);
+}
+void Map_delete_line(Map *map,int y){
+    if(map->height==1){
+        return;
+    }
+    map->height--;
+    int bwidth=(map->width+1)>>1;
+    UINT8*new_data=malloc(bwidth*map->height);
+    UINT8*old_data=map->data;
+    for(int i=0;i<y*bwidth;i++){
+        new_data[i]=old_data[i];
+    }
+    for(int i=0;i<(map->height-y)*bwidth;i++){
+        new_data[i+y*bwidth]=old_data[i+(y+1)*bwidth];
+    }
+    map->data=new_data;
+    free(old_data);
 }
 Map Map_Create(char* map){
     Map result={0,0,NULL,0};
@@ -80,7 +196,7 @@ Map Map_Create(char* map){
     nb+=0x400;
     result.width=nb&0x3ff;
     result.height=(nb&0xffc00)>>10;
-    result.data=malloc(((result.height+1)>1)*result.height);
+    result.data=malloc(((result.width+1)>1)*result.height);
     for(int i=0,j=0,k=0;map[i]!='!';i++){
         if(map[i]=='#'){
             j=0;

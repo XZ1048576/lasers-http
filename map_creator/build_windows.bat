e59994f6becfd623e675f1f8d129f47aa1533031
XZ1048576 <sylvain.lovis@gmail.com>
2022-12-21 14:42:04 +0100

    remove map creator files

--- map_creator/build_windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-replace360000a.py windows
-cd build_windows
-gcc map_creator.c resources.res -o map_creator.exe -L./lib -I./../include -lmingw32 -lSDL2main -lSDL2 -lcomdlg32 %1
-cd ..
-replace_pixels.py windows
-pause
